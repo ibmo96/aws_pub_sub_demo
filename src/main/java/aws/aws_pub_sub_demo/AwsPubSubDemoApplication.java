09ddd3ba7236e682739e9a790deae911e19eaca4
Ibrahim Mohammad <ibrahim.mohammad96@hotmail.com>
2020-10-06 16:05:31 +0200

    first commit

---
+++ src/main/java/aws/aws_pub_sub_demo/AwsPubSubDemoApplication.java
@@ -0,0 +1,13 @@
+package aws.aws_pub_sub_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsPubSubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsPubSubDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aws/aws_pub_sub_demo/AwsPubSubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package aws.aws_pub_sub_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPubSubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
